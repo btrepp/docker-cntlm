89ca71414f48d797320eb973bf3aadc3f5b3a071
Beau Trepp <beau.trepp@riotinto.com>
2014-10-13 13:08:27 +0800

    Don't forward local addresses to upstream proxy, and don't forward anything on the same domain

--- cntlm_start.sh
+++ cntlm_start.sh
@@ -3,12 +3,12 @@ if [ $# -ne 4 ]; then
 	echo "Usage: username domain NTLMv2hash upstream_proxy"
         exit 1
 fi
-rm /etc/cntlm.ini
 echo UserName $1 >> /etc/cntlm.ini
 echo Domain $2 >> /etc/cntlm.ini
 echo PassNTLMv2 $3 >> /etc/cntlm.ini
 echo Proxy $4 >> /etc/cntlm.ini
+echo NoProxy localhost, 127.0.0.*, 10.*, 192.168.*, *.$2 >> /etc/cntlm.ini
 echo Gateway yes >> /etc/cntlm.ini
 echo Listen 3128 >> /etc/cntlm.ini
 
-exec /usr/sbin/cntlm -U cntlm -c /etc/cntlm.ini
+exec /usr/sbin/cntlm -f -U cntlm -c /etc/cntlm.ini
